4c5e067671cca64b9b1896274147cc17fd0dd752
DezeZhao <1360536767@qq.com>
2020-04-15 01:04:25 +0800

    类作为成员变量

---
+++ src/JavaBasic/test6/Demo06/DemoMain.java
@@ -0,0 +1,23 @@
+package JavaBasic.test6.Demo06;
+
+//类对象作为成员变量
+//类作为成员变量时，对它进行赋值的操作，实际上，是赋给它该类的一个对象。
+public class DemoMain {
+
+    public static void main(String[] args) {
+        // 创建一个英雄角色
+        Hero hero = new Hero();
+        // 为英雄起一个名字，并且设置年龄
+        hero.setName("盖伦");
+        hero.setAge(20);
+
+        // 创建一个武器对象
+        Weapon weapon = new Weapon("AK-47");
+        // 为英雄配备武器
+        hero.setWeapon(weapon);
+
+        // 年龄为20的盖伦用多兰剑攻击敌方。
+        hero.attack();
+    }
+
+}
